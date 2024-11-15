88702e9aa76822f04ae0e677cc35f1ab83759df9
Max M'BEY <mbey.e2100648@etud.univ-ubs.fr>
2024-11-15 21:11:28 +0100

    Déclaration retirerCarte() et ajouterCarte()

--- Joueur.java
+++ Joueur.java
@@ -2,21 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Joueur{
-    private List<Carte> main;
+    private ArrayList<Carte> main;
     private String nom;
     private int kilometre;
     //private Etat etat;
     private int id;
 
-    public Joueur(String n, int k, int i){
+    public Joueur(String name, int km, int id){
         main = new ArrayList<Carte>();
-        nom = n;
-        kilometre = k;
+        nom = name;
+        kilometre = km;
         //etat = null;
-        id = i;
+        id = this.id;
     }
 
-    public List<Carte> getMain(){
+    public ArrayList<Carte> getMain(){
         return main;
     }
     public String getNom(){
@@ -32,11 +32,20 @@ public abstract class Joueur{
         return id;
     }
 
-    public void piocher(){};
-    public boolean verification(Carte c, Joueur u, Joueur cible){return true;};
-    public void jouerCarte(Carte c){};
-    public void appliquerAction(Carte c){};
-    public void retirerCarte(Carte c){};
-    public void ajouterCarte(Carte c){};
+    public void piocher(){}
 
+    public boolean verification(Carte c, Joueur u, Joueur cible){
+        return true;
+    }
+    public void jouerCarte(Carte c){}
+
+    public void appliquerAction(Carte c){}
+
+    public void retirerCarte(Carte c){
+        main.remove(c);
+    }
+
+    public void ajouterCarte(Carte c){
+        main.add(c);
+    }
 }
