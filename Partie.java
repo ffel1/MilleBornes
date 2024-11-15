0a6b327756dbc9b25cbdf51cd227dee207b4066d
Enzodero17 <enzo.dero.04@gmail.com>
2024-11-15 12:03:16 +0100

    Menu OK

--- Partie.java
+++ Partie.java
@@ -15,58 +15,69 @@ import javax.swing.JPanel;
 public class Partie {
     private int points;
     private List<Joueur> joueurs;
+    private static JFrame fenetreMenu;
 
     public Partie(){
         points = 0;
         joueurs = new ArrayList<Joueur>();
-    }
+    } 
 
     public static void main(String[] args){
-        JFrame fenetre = new JFrame();
-        fenetre.setVisible(true);
-        JPanel panel = new JPanel();
-        fenetre.setContentPane(panel);
+        creerFenetre();
+    }
+
+    /*
+     * Affiche la fenetre du menu de début
+     */
+    private static void creerFenetre(){
+        fenetreMenu = new JFrame("1000 Bornes");
 
         // Fentre de la taille de l'écran
         Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
         int hauteur = (int)dimension.getHeight();
         int largeur  = (int)dimension.getWidth();
-        fenetre.setSize(largeur, hauteur);
-        panel.setLayout(new BorderLayout());
+        fenetreMenu.setSize(largeur, hauteur);
 
         // Bouton jouer
-        JPanel boutonJouerPanel = new JPanel();
-        boutonJouerPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-        panel.add(boutonJouerPanel, BorderLayout.NORTH);
+        JPanel jouerPanel = new JPanel();
+		jouerPanel.setBounds(0, 0, largeur, hauteur / 5);
+		jouerPanel.setLayout(new BorderLayout());
+        fenetreMenu.add(jouerPanel);
+        JLabel labelJouer = new JLabel();
         JButton boutonJouer = new JButton("Jouer");
-        boutonJouer.setVisible(true);
-        boutonJouer.setPreferredSize(new Dimension(150, 50));
-        //boutonJouer.addActionListener(e -> System.out.println("Bouton jouer"));
-        boutonJouerPanel.add(boutonJouer, BorderLayout.CENTER);
-
-        //  Bouton quitter
-        JPanel boutonPanel = new JPanel();
-        boutonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-        panel.add(boutonPanel, BorderLayout.SOUTH);
-        JButton boutonQuitter = new JButton("Quitter");
-        boutonQuitter.setVisible(true);
-        boutonQuitter.setPreferredSize(new Dimension(150, 50));
-        boutonQuitter.addActionListener(e -> System.exit(0));
-        boutonPanel.add(boutonQuitter, BorderLayout.CENTER);
+        boutonJouer.setBounds(largeur / 2 - 75, hauteur / 5 - 50, 150, 50);
+        labelJouer.add(boutonJouer);
+        jouerPanel.add(labelJouer);
 
         // Image
-        ImageIcon image = new ImageIcon("MilleBornes.png");
-        JLabel label = new JLabel(image);
-        label.setVisible(true);
         JPanel imagePanel = new JPanel();
-        imagePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-        panel.add(imagePanel, BorderLayout.CENTER);
-        imagePanel.add(label);
+		imagePanel.setBounds(0, hauteur / 5, largeur, 3 * hauteur / 5);
+		imagePanel.setLayout(new BorderLayout());
+        fenetreMenu.add(imagePanel);
+        ImageIcon image = new ImageIcon("MilleBornes.png");
+        JLabel labelImage = new JLabel();
+        labelImage.setIcon(image);
+        labelImage.setVerticalAlignment(JLabel.CENTER);
+        labelImage.setHorizontalAlignment(JLabel.CENTER);
+        imagePanel.add(labelImage);
 
-        boutonPanel.setBackground(Color.GREEN);
-        boutonJouerPanel.setBackground(Color.GREEN);
-        //panel.add(label, BorderLayout.CENTER);
-        panel.setBackground(Color.ORANGE);
+        // Bouton quitter
+        JPanel quitterPanel = new JPanel();
+		quitterPanel.setBounds(0, hauteur * 4 / 5, largeur, hauteur / 5);
+		quitterPanel.setLayout(new BorderLayout());
+        JLabel labelQuitter = new JLabel();
+        JButton boutonQuitter = new JButton("Quitter");
+        boutonQuitter.setBounds(largeur / 2 - 75, 0, 150, 50);
+        boutonQuitter.addActionListener(e -> System.exit(0));
+        labelQuitter.add(boutonQuitter);
+        quitterPanel.add(labelQuitter);
+
+        fenetreMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetreMenu.setLayout(null);
+		fenetreMenu.setVisible(true);	
+		fenetreMenu.add(jouerPanel);
+		fenetreMenu.add(imagePanel);
+		fenetreMenu.add(quitterPanel);
     }
 
     public void nouvellePartie(){};
