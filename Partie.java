7fcbed370bf1e128a6bcc13838558d0fa8e5a287
Max M'BEY <mbey.e2100648@etud.univ-ubs.fr>
2024-11-16 00:33:43 +0100

    Creation et initialisation ArrayList pioche

--- Partie.java
+++ Partie.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.util.ArrayList;
-
+import java.util.Collections;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -14,14 +14,17 @@ import javax.swing.JPanel;
 public class Partie {
     private int points;
     private ArrayList<Joueur> joueurs;
+    private static ArrayList<Carte> pioche;
     private static JFrame fenetreMenu;
 
     public Partie(){
         points = 0;
         joueurs = new ArrayList<Joueur>();
+        pioche = new ArrayList<Carte>();
     } 
 
     public static void main(String[] args){
+        initialiserPioche();
         creerFenetre();
     }
 
@@ -79,6 +82,56 @@ public class Partie {
 		fenetreMenu.add(quitterPanel);
     }
 
+    private static void initialiserPioche(){
+        //Cartes Distance
+        for(int i = 0;i<12;i++){
+            if(i<10){
+                pioche.add(new Distance(TypeCarte._25KM, 25));
+                pioche.add(new Distance(TypeCarte._50KM, 50));
+                pioche.add(new Distance(TypeCarte._75KM, 75));
+
+                if(i<8){
+                    pioche.add(new Distance(TypeCarte._200KM, 200));
+                }
+            }
+            pioche.add(new Distance(TypeCarte._100KM, 100));
+        }
+
+        //Cartes Attaque
+        for(int i = 0;i<6;i++){
+            if(i<5){
+                pioche.add(new Attaque(TypeCarte.FEU_ROUGE));
+
+                if(i<3){
+                    pioche.add(new Attaque(TypeCarte.ACCIDENT));
+                    pioche.add(new Attaque(TypeCarte.PANNE_D_ESSENCE));
+                    pioche.add(new Attaque(TypeCarte.CREVAISON));
+                }
+            }
+            pioche.add(new Attaque(TypeCarte.LIMITATION_DE_VITESSE));
+        }
+
+        //Cartes Parade
+        for(int i = 0;i<14;i++){
+            if(i<6){
+                pioche.add(new Parade(TypeCarte.REPARATION));
+                pioche.add(new Parade(TypeCarte.ESSENCE));
+                pioche.add(new Parade(TypeCarte.ROUE_DE_SECOURS));
+                pioche.add(new Parade(TypeCarte.FIN_LIMITATION_VITESSE));
+            }
+            pioche.add(new Parade(TypeCarte.FEU_VERT));
+        }
+
+        //Cartes Botte
+        pioche.add(new Botte(TypeCarte.AS_DU_VOLANT));
+        pioche.add(new Botte(TypeCarte.CAMION_CITERNE));
+        pioche.add(new Botte(TypeCarte.INCREVABLE));
+        pioche.add(new Botte(TypeCarte.VEHICULE_PRIORITAIRE));
+
+        //Melanger la pioche
+        Collections.shuffle(pioche);
+    }
+        
     public void nouvellePartie(){};
     public void quitterPartie(){};
     public void sauvegarderPartie(){};
@@ -89,4 +142,6 @@ public class Partie {
     public void afficherAction(Carte c, Joueur u, Joueur cible){};
     public void ajouterHistorique(Carte c, Joueur u, Joueur cible){};
     public void nouvelleManche(){};
+
+    
 }
