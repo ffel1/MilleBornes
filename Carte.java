195ecc67ce59e935e3ef47038521117aa5f25131
Max M'BEY <mbey.e2100648@etud.univ-ubs.fr>
2024-11-17 01:13:37 +0100

    Ajout attribut ImageIcon et initialisation

--- Carte.java
+++ Carte.java
@@ -1,16 +1,26 @@
+import javax.swing.ImageIcon;
+
 public abstract class Carte {
     private String nom;
     private TypeCarte type;
+    private ImageIcon image;
 
+    //Constructeur avec nom, type et ImageIcon image pour les cartes
     public Carte(String n, TypeCarte t){
         nom = n;
         type = t;
+        image = new ImageIcon("images/"+t.toString()+".png");
     }
 
     public String getNom(){
         return nom;
     }
+
     public TypeCarte getType(){
         return type;
+    }
+
+    public ImageIcon getImage(){
+        return image;
     }
 }
