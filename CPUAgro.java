3f8804f0f3e9ae6f8077f6a9428b02242b993643
Felix <felix.cloerec5@gmail.com>
2024-11-22 09:19:01 +0100

    modif CPUAgro

--- CPUAgro.java
+++ CPUAgro.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class CPUAgro extends CPU{
 
     public CPUAgro(String nom, int k, int id){
@@ -7,9 +9,26 @@ public class CPUAgro extends CPU{
     /*
      * Choisi de jouer une carte, en priorité des carte attaques
      */
-    public void choisirCarte(){
-        // A Completer
-        jouerCarte(null);
+    public void choisirCarte(Carte c){
+        ArrayList<Carte> main = getMain();
+        Carte carteAJouer = null;
+
+        for (Carte carte : main) {
+            if (carte instanceof Attaque){
+                carteAJouer = carte;
+                break;
+            }
+        }
+
+        if (carteAJouer == null && !main.isEmpty()){
+            
+        }
+
+        if (carteAJouer != null){
+            jouerCarte(carteAJouer);
+            appliquerAction(carteAJouer);
+            retirerCarte(carteAJouer);
+        }
     }
 
     public void appliquerAction(Carte c){};
