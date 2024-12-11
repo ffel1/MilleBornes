2daf4240ed378c7c1f49309d19ed808d1efcb73f
Enzo Dero <enzo.dero.04@gmail.com>
2024-12-11 10:10:00 +0100

    Son bon

--- src/main/Son.java
+++ src/main/Son.java
@@ -1,60 +1,41 @@
 package main;
 
+import java.net.URL;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import java.net.URL;
+import java.io.File;
 
-public class Son {
+public class Son{
     
     private Clip clip;
-    private URL[] sonURLs = new URL[30];  // Changed from File[] to URL[]
+    private File sonURL[] = new File[30];
 
-    public Son() {
-        // Initialize the array with sound files as URLs
-        sonURLs[0] = getClass().getResource("/son/Foule.wav");  // Use relative path or absolute URL
-        sonURLs[1] = getClass().getResource("/son/Klaxon.wav");
-        // Add more files as needed
+    public Son(){ // Ajouter d'autre son ici
+        sonURL[0] = new File("son/Foule.wav");
+        sonURL[1] = new File("son/Klaxon.wav");
     }
 
-    public void setFile(int i) {
-        try {
-            // Check if the URL is null (file might not be found)
-            if (sonURLs[i] == null) {
-                System.out.println("Sound file not found for index: " + i);
-                return;
-            }
-
-            // Load the audio input stream from the URL
-            AudioInputStream ais = AudioSystem.getAudioInputStream(sonURLs[i]);
+    public void setFile(int i){
+        try{
+            AudioInputStream ais = AudioSystem.getAudioInputStream(sonURL[i]);
             clip = AudioSystem.getClip();
-            clip.open(ais);
-        } catch (Exception e) {
+            clip.open(ais);  
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
 
-    public void play() {
-        if (clip != null) {
-            clip.start();
-        } else {
-            System.out.println("No sound loaded to play.");
-        }
+    public void play(){
+        clip.start();
     }
 
-    public void loop() {
-        if (clip != null) {
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-        } else {
-            System.out.println("No sound loaded to loop.");
-        }
+    public void loop(){
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
 
-    public void stop() {
-        if (clip != null) {
-            clip.stop();
-        } else {
-            System.out.println("No sound loaded to stop.");
-        }
+    public void stop(){
+        clip.stop();
     }
 }
